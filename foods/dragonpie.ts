60ab07bacd9c1940cfe3695875a1cb589f61faa9
imba97 <mail@imba97.cn>
2025-02-04 19:25:28 +0800

    feat: add dragonpie recipe

---
+++ foods/dragonpie.ts
@@ -0,0 +1,33 @@
+import type { ComparisonOperator } from '~/types/comparisonOperator'
+import type { IngredientsCondition } from '~/types/ingredientsCondition'
+import icon from '~/assets/images/foods/dragonpie.png'
+import { FoodBase } from '~/composables/food/foodBaseClass'
+import DragonFruit from '~/ingredients/dragonFruit'
+import PreparedDragonFruit from '~/ingredients/preparedDragonFruit'
+
+export default class Dragonpie extends FoodBase {
+  _name = '火龙果派'
+  _health = 20
+  _hunger = 75
+  _sanity = 5
+  _rot = 15
+  _cooking = 40
+  _priority = 1
+  _image = icon
+
+  protected override _ingredientsCondition: IngredientsCondition[] = [
+    {
+      ingredients: [
+        DragonFruit,
+        PreparedDragonFruit
+      ],
+      condition: {
+        ge: 1
+      }
+    }
+  ]
+
+  protected override _meat: ComparisonOperator = {
+    eq: 1
+  }
+}
