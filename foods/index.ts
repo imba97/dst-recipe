c37bdaf531781e7dba1e9d999e5a34dbea5c7fbc
imba97 <mail@imba97.cn>
2025-02-04 23:35:27 +0800

    refacotr: change pinyin library

--- foods/index.ts
+++ foods/index.ts
@@ -1,4 +1,4 @@
-import pinyin from 'tiny-pinyin'
+import pinyin from 'pinyin'
 
 const foodsModule = import.meta.glob('./*.ts', { eager: true }) as Record<string, { default: FoodBaseConstructor }>
 
@@ -6,12 +6,14 @@ const foods = Object.keys(foodsModule).map((path) => {
   const FoodBase = foodsModule[path].default
   const food = useFood(FoodBase)
 
-  const pinyinName = pinyin.convertToPinyin(food.name, ',', true)
-  const pinyinNameSplit = pinyinName.split(',')
-  const pinyinInitials = pinyinNameSplit.map(item => item[0]).join('')
+  const pinyinName = _map(pinyin(food.name, {
+    style: pinyin.STYLE_NORMAL
+  }), ([first]) => first)
+
+  const pinyinInitials = pinyinName.map(word => word[0]).join('')
 
   food.pinyin = {
-    name: pinyinNameSplit.join(''),
+    name: pinyinName.join(''),
     initials: pinyinInitials
   }
 
